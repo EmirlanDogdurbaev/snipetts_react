c4fa9358524c4a737da10f447c3ff0a5c8e9a76c
EmirlanDogdurbaev <jigit38@gmail.com>
2024-05-01 15:32:38 +0600

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Register from "./components/Auth/Register/Register.jsx";
 import Login from "./components/Auth/Login/Login.jsx";
 import Logout from "./components/Auth/Logout/Logout.jsx";
 
 function App() {
-  return (
-    <>
+    return (
+        <>
+            <div>
+                <Logout/>
+                <Routes>
+                    <Route path="/register" element={<Register/>}/>
+                    <Route path="/" element={<Login/>}/>
+                </Routes>
+            </div>
 
-        <div>
-            <Logout/>
-          <Routes>
-            <Route path="/register" element={<Register />} />
-            <Route path="/" element={<Login />} />
-          </Routes>
-        </div>
-
-    </>
-  );
+        </>
+    );
 }
 
 export default App;
